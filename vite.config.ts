da37872a5be5aa18972fdde15da4a8c238e22cad
Josie Ko <josipakukor@gmail.com>
2025-05-19 13:44:59 +0200

    Fix dependency issues

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,6 @@ export default defineConfig({
         },
       },
     },
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: "esbuild",
   },
 });
