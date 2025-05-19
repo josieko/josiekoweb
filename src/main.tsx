a425a2c5a8514bc2038691364d2c2b4d44527504
Josie Ko <josipakukor@gmail.com>
2025-05-19 13:22:05 +0200

    Remove unused assets

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import emailjs from "@emailjs/browser";
-
-emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
